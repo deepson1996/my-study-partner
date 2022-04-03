80d0dcd9864e5d21cdf9821e02e61c264f1449cd
pokhreldipshan1996 <pokhreldipshan1996@gmail.com>
2022-04-03 14:07:38 +0545

    App specific urls.py and included it in main urls file

--- base/views.py
+++ base/views.py
@@ -1,3 +1,9 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+def home(request): # for one page as homepage
+    return HttpResponse('Hello World!')
+
+def room(request):
+    return HttpResponse('Hello Room!') # another page in /room
--- stydybud/urls.py
+++ stydybud/urls.py
@@ -1,15 +1,9 @@
 from django.contrib import admin
-from django.urls import path
-from django.http import HttpResponse
+from django.urls import path, include
 
 
-def home(request): # for one page as homepage
-    return HttpResponse('Hello World!')
 
-def room(request):
-    return HttpResponse('Hello Room!') # another page in /room
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home),
-    path('room/', room),
+    path('', include('base.urls')),
 ]
